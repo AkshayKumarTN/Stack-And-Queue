893434e85f03a530a0df8ada18be7cb4b5b93625
AkshayKumarTN <tnak369@gmail.com>
2021-07-10 19:56:00 +0530

    [Akshay] initial Commit

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Stack\n");
+            
+
+        }
+    }
+}
